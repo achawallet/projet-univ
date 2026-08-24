831d83915c3fbaf1d0da5867bdc476bca686e84f
Aicha <aichawallet385@gmail.com>
2026-08-24 12:29:00 +0000

    initialisation du projet projet-univ

---
+++ src/main/java/org/example/projetuniv/ProjetUnivApplication.java
@@ -0,0 +1,13 @@
+package org.example.projetuniv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetUnivApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetUnivApplication.class, args);
+    }
+
+}
